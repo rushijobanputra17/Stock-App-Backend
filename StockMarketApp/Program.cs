a958cdc1e6acd733a922e559404ee4f5b953a1ea
Jobanputra Rushi <61899855+rushijobanputra@users.noreply.github.com>
2025-02-20 22:24:24 +0530

    Update Program.cs

--- StockMarketApp/Program.cs
+++ StockMarketApp/Program.cs
@@ -145,6 +145,7 @@ app.MapControllers();
 
 app.Run();
 
+
 //internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 //{
 //    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
